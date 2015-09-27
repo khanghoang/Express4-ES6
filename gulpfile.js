5384c9010c464d146841d682bf26808354b1437c
KhangHoang <hoangtrieukhang@gmail.com>
2015-09-27 13:28:25 +0700

    gulp file nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,23 @@ var babel = require("gulp-babel");
 var concat = require("gulp-concat");
 var nodemon = require('gulp-nodemon');
 
-gulp.task("default", function () {
+gulp.task("babel", function () {
   return gulp.src("app/**/*.js")
   .pipe(sourcemaps.init())
   .pipe(babel())
   .pipe(concat("all.js"))
   .pipe(sourcemaps.write("."))
-  .pipe(gulp.dest("dist"));
+  .pipe(gulp.dest("dist"))
 });
 
 
 
-gulp.task('start', function() {
+gulp.task('default', function() {
   nodemon({
       script: 'dist/all.js'
+    , watch: 'app/'
     , ext: 'js html'
     , env: { 'NODE_ENV': 'development' }
+    , tasks: ['babel']
     })
 })
