e3110b96718ffee8b7e4c2b6cd96d375cb542eb7
KhangHoang <hoangtrieukhang@gmail.com>
2015-10-12 11:28:47 +0700

    correct the general error handling
    bsae on this documation http://expressjs.com/guide/error-handling.html, middleware to handle error NEED to the after other app.use and routes calls.

--- app/server.js
+++ app/server.js
@@ -3,10 +3,15 @@ import Bar from './bar';
 import connectToDatabase from './config/database';
 import mongoose from 'mongoose';
 import config from './config/config';
+import bodyParser from 'body-parser';
+import methodOverride from 'method-override';
 
 const app = express();
 
 connectToDatabase(app, mongoose);
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride());
 
 app.get('/', (req, res) => {
   res.send('Hello world' + Bar.foo);
@@ -21,19 +26,24 @@ app.get('/somewierdurl', (req, res, next) => {
 });
 
 // handle error
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
+  if (!err) {
+    next();
+  }
+
   let status = err.status || 500;
   let message = err.message || 'Opps, there was an error';
 
+  res.status(status);
+
   // handle html
   if (req.accepts('html')) {
-    return res.status(status).send(message);
+    return res.send(message);
   }
 
   // handle json
-  if (req.accepts('json')) {
-    return res.status(status)
-    .json({
+  if (req.accepts('application/json')) {
+    return res.json({
       status: status,
       message: message
     });
