ebcec7f3a32972f21a1533042cfcdab076a6895c
KhangHoang <hoangtrieukhang@gmail.com>
2015-10-15 07:11:38 +0700

    using fat arrow to keep 'this' context

---
+++ app/controllers/UserController.js
@@ -0,0 +1,16 @@
+import Promise from 'bluebird';
+
+class UserController {
+  constructor(userClass) {
+    this.userClass = Promise.promisifyAll(userClass);
+  }
+
+  index = async(req, res) => {
+    let User = this.userClass;
+    var users = [];
+    users = await User.findAsync({});
+    return res.status(200).json({data: users});
+  }
+}
+
+export default UserController;
