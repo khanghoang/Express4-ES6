afef2f3935b510e0ac088e2effc3bbc622709695
KhangHoang <hoangtrieukhang@gmail.com>
2015-10-18 14:43:43 +0700

    add passport strategies

---
+++ app/oauth/passport.js
@@ -0,0 +1,68 @@
+import passport from 'passport';
+import {Strategy as LocalStrategy} from 'passport-local';
+import {Strategy as BasicStrategy} from 'passport-http';
+import {Strategy as ClientPasswordStrategy} from 'passport-oauth2-client-password';
+import {Strategy as BearerStategy} from 'passport-http-bearer';
+
+passport.user(new LocalStrategy(
+  function(username, password, done) {
+    UserManager.findByUsername(username, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (user.password !== password ) {
+        return done(null, false);
+      }
+
+      return done(null, user);
+    });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  UserManager.find(id, function(id, done) {
+    done(err, user);
+  });
+})
+
+passport.use(new BasicStrategy(
+  function(username, password, done) {
+    ClientManager.findByClientId(username, function(err, client) {
+      if (err) { return done(err); }
+      if (!client) { return done(null, false); }
+      if (client.clientSecret != password) { return done(null, false); }
+      return done(null, client);
+    });
+  }
+));
+
+passport.use(new ClientPasswordStrategy(
+  function(clientId, clientSecret, done) {
+    ClientManager.findByClientId(clientId, function(err, client) {
+      if (err) { return done(err); }
+      if (!client) { return done(null, false); }
+      if (client.clientSecret != clientSecret) { return done(null, false); }
+      return done(null, client);
+    });
+  }
+));
+
+passport.use(new BearerStrategy(
+  function(accessToken, done) {
+    AccessTokenManager.find(accessToken, function(err, token) {
+      if (err) { return done(err); }
+      if (!token) { return done(null, false); }
+      UserManager.find(token.userID, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        // to keep this example simple, restricted scopes are not implemented,
+        // and this is just for illustrative purposes
+        var info = { scope: '*' }
+        done(null, user, info);
+      });
+    });
+  }
+));
