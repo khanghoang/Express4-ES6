f53653de7b67d0b5c6c3f71ebea76397b208d7a7
KhangHoang <hoangtrieukhang@gmail.com>
2015-10-25 22:48:49 +0700

    change the design end point

--- app/routers/DesignControllerRouter.js
+++ app/routers/DesignControllerRouter.js
@@ -1,15 +1,21 @@
 import {Router as router} from 'express';
-import DesignController from '../controllers/api/v1/DesignController';
+import DesignAPIController from '../controllers/api/v1/DesignController';
+import DesignController from '../controllers/DesignController';
 
 class DesignControllerRouter {
   static route() {
     this.router = router();
 
-    // this is the route
-    this.router.post('/v1/api/design/upload', DesignController.uploadDesign());
-    this.router.get('/v1/api/design', DesignController.getDesigns);
+    // this is the API
+    this.router.post('/v1/api/design/upload',
+                     DesignAPIController.uploadDesign());
+    this.router.get('/v1/api/design',
+                    DesignAPIController.getDesigns);
     this.router.get('/v1/api/design/approved',
-                    DesignController.getAllApprovedDesigns);
+                    DesignAPIController.getAllApprovedDesigns);
+
+    // this is for CMS
+    this.router.get('/admin/designs', DesignController.index);
 
     return this.router;
   }
