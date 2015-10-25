9b7352b950adfd65b18053e83d5d611a5858350c
KhangHoang <hoangtrieukhang@gmail.com>
2015-10-25 22:26:51 +0700

    refactor code

--- app/managers/DesignManager.js
+++ app/managers/DesignManager.js
@@ -22,26 +22,31 @@ class DesignManager {
       return await design.save();
     }
 
-  static getAllApprovedDesigns : Promise = (params: Object, query: Object) => {
-    return new Promise(function(resolve, reject) {
-      Designs.paginate(
-        query,
-        {
-          page: params.page || 0,
-          limit: params.limit || 50
-        },
-        function(err, designs, pageCount, itemCount) {
-          if (err) {
-            return reject(err);
-          }
+  static getDesignsWithPagination : Promise =
+    (params: Object, query: Object) => {
+      return new Promise(function(resolve, reject) {
+        Designs.paginate(
+          query,
+          {
+            page: params.page || 0,
+            limit: params.limit || 50
+          },
+          function(err, designs, pageCount, itemCount) {
+            if (err) {
+              return reject(err);
+            }
 
-          return resolve({
-            data: designs,
-            pageCount: pageCount,
-            itemCount: itemCount
+            return resolve({
+              data: designs,
+              pageCount: pageCount,
+              itemCount: itemCount
+            });
           });
-        });
-    });
+      });
+    };
+
+  static getAllApprovedDesigns : Promise = (params: Object, query: Object) => {
+    return DesignManager.getDesignsWithPagination(params, query);
   };
 }
 
