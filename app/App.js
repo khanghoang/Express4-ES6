286c6a82b0c38b1f437bb66d0acd37cfc88a5e19
KhangHoang <hoangtrieukhang@gmail.com>
2015-10-26 15:16:06 +0700

    fix CORS

--- app/App.js
+++ app/App.js
@@ -143,9 +143,10 @@ class App {
 
     var allowCrossDomain = function(req, res, next) {
       res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
       res.header('Access-Control-Allow-Credentials', true);
       res.header('Access-Control-Allow-Headers',
-                 'X-Requested-With,content-type');
+                 'Content-Type, Authorization, Content-Length, X-Requested-With');
       next();
     };
     this.express.use(allowCrossDomain);
